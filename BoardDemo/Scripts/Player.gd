eb4d6c351c4f545369dabbf3b1aa878a61010b73
Cirnol <cn6495@yahoo.com>
2021-04-22 17:10:04 -0700

    Reactive script attached to all reactive objects

--- BoardDemo/Scripts/Player.gd
+++ BoardDemo/Scripts/Player.gd
@@ -30,7 +30,7 @@ func _process(delta):
 		return
 	if Input.is_action_pressed("ui_roll") && !moving && active && !spinner.playing():
 		#roll = rng.randi_range(1, 10)
-		roll = 4
+		roll = 6
 		spinner.play(roll)
 		yield(spinner, "spinner_done")
 		moving = true
